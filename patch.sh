edf6d11e23f8342c8c47ff9d91010bd078532662
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-12-17 15:16:39 +0900

    Update patch.sh
    fix script error

--- patch.sh
+++ patch.sh
@@ -8,7 +8,7 @@ echo ${targetDir}
 
 echo "uncompress rookie configurations"
 cp ./rookie.tar.gz ../patch_nexell_nougat_r1/device/nexell/
-cd ../targetDir/device/nexell/
+cd targetDir/device/nexell/
 tar -xvzf ./rookie.tar.gz
 rm ./rookie.tar.gz
 cd ${srcDir}
@@ -16,21 +16,21 @@ cd ${srcDir}
 
 echo "apply patch to bl1-s5p6818"
 cp -R ./bl1-s5p6818/* ../patch_nexell_nougat_r1/device/nexell/bl1/bl1-s5p6818/
-cd ../targetDir/device/nexell/bl1/bl1-s5p6818/
+cd targetDir/device/nexell/bl1/bl1-s5p6818/
 patch -p1 < ./bl1-s5p6818.patch
 rm ./bl1-s5p6818.patch
 cd ${srcDir}
 
 echo "apply patch to u-boot"
 cp -R ./u-boot/* ../patch_nexell_nougat_r1/device/nexell/u-boot/u-boot-2016.01/
-cd ../targetDir/device/nexell/u-boot/u-boot-2016.01/
+cd targetDir/device/nexell/u-boot/u-boot-2016.01/
 patch -p1 < ./u-boot.patch
 rm u-boot.patch
 cd ${srcDir}
 
 echo "apply patch to kernel"
 cp -R ./kernel/* ../patch_nexell_nougat_r1/device/nexell/kernel/kernel-4.4.x/
-cd ../targetDir/device/nexell/kernel/kernel-4.4.x/
+cd targetDir/device/nexell/kernel/kernel-4.4.x/
 patch -p1 < ./kernel-4.4.x.patch
 rm kernel-4.4.x.patch
 cd ${srcDir}
