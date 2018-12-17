69c5fe053db5ee4d2a90e104004d810be3363f41
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2018-12-17 12:29:51 +0900

    create recovery.sh
    create recovery.sh

---
+++ recovery.sh
@@ -0,0 +1,11 @@
+echo "bl1-s5p6818 recovery"
+rm -rf device/nexell/bl1/bl1-s5p6818
+repo sync -l device/nexell/bl1/bl1-s5p6818
+
+echo "u-boot recovery"
+rm -rf device/nexell/u-boot/u-boot-2016.01/
+repo sync -l device/nexell/u-boot/u-boot-2016.01/
+
+echo "kernel recovery"
+rm -rf device/nexell/kernel/kernel-4.4.x/
+repo sync -l device/nexell/kernel/kernel-4.4.x/
